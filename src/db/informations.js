f82761ee5b03668514836736087cda082296b9be
Beniamin Guras <gurasbeniamin@gmail.com>
2022-09-12 13:40:51 +0200

    add new project

--- src/db/informations.js
+++ src/db/informations.js
@@ -1,96 +1,119 @@
 export const personal = [
   {
-    type: 'text',
-    title: 'My story',
-    description: 'I\'ve been in love with video games from the very beginning. At the age of 7, I was making my first maps and "mods" for games like Half Life. Because of this, I became interested in things like computers - and that\'s how it starts. After computers, there were cell phones (I still remember when my dad bought the first Nokia with a color display). When I was 9 years old I built my first computer. I didn\'t know it at the time, but at that point I already found a passion for life. I needed to know everything about technology and how it works - so the next step was natural ... Programming! And so I am here where I am ...'
+    type: "text",
+    title: "My story",
+    description:
+      "I've been in love with video games from the very beginning. At the age of 7, I was making my first maps and \"mods\" for games like Half Life. Because of this, I became interested in things like computers - and that's how it starts. After computers, there were cell phones (I still remember when my dad bought the first Nokia with a color display). When I was 9 years old I built my first computer. I didn't know it at the time, but at that point I already found a passion for life. I needed to know everything about technology and how it works - so the next step was natural ... Programming! And so I am here where I am ...",
   },
   {
-    type: 'list',
-    title: 'Hobbies',
-    list: ['Technologies', 'Games', 'Motorcycles', 'Good Food', 'Basketball', 'History']
-  },
-  {
-    type: 'list',
-    title: 'Favorite games',
+    type: "list",
+    title: "Hobbies",
     list: [
-      'Skyrim',
-      'Mount&Blade: Bannerlord',
-      'God of War',
-      'Diablo 3',
-      'League of Legends',
-      'Counter Strike Global Offensive',
-      'Pokemon GO'
-    ]
+      "Technologies",
+      "Games",
+      "Motorcycles",
+      "Good Food",
+      "Basketball",
+      "History",
+    ],
   },
   {
-    type: 'text',
-    title: 'Ambitions, plans, dreams',
-    description: 'I would love to visit the USA, maybe for a little bit longer - play with real Amercian basketball team and eat a hamburger from the bar.'
-  }
-]
+    type: "list",
+    title: "Favorite games",
+    list: [
+      "Skyrim",
+      "Mount&Blade: Bannerlord",
+      "God of War",
+      "Diablo 3",
+      "League of Legends",
+      "Counter Strike Global Offensive",
+      "Pokemon GO",
+    ],
+  },
+  {
+    type: "text",
+    title: "Ambitions, plans, dreams",
+    description:
+      "I would love to visit the USA, maybe for a little bit longer - play with real Amercian basketball team and eat a hamburger from the bar.",
+  },
+];
 
 export const professional = [
   {
-    type: 'text',
-    title: 'Programming skills',
-    description: 'I wrote my first code when I was 11. Since then, I\'ve had plenty of opportunities to code in multiple languages. I graduated from high school with an IT profile - there I had the opportunity to work with frontend technologies, over there I learned HTML, JavaScript and CSS. Then I focused on C ++ and Python. A few years later, I signed myself for the Frontend React developer bootcamp where I added React, React Router and Redux to my skills list.',
+    type: "text",
+    title: "Programming skills",
+    description:
+      "I wrote my first code when I was 11. Since then, I've had plenty of opportunities to code in multiple languages. I graduated from high school with an IT profile - there I had the opportunity to work with frontend technologies, over there I learned HTML, JavaScript and CSS. Then I focused on C ++ and Python. A few years later, I signed myself for the Frontend React developer bootcamp where I added React, React Router and Redux to my skills list.",
   },
   {
-    type: 'text',
-    title: 'Experience',
-    description: 'For three years I worked as a Logistics Specialist in one of the largest automotive companies (Simoldes Plasticos). Programming skills allowed me to automate logistics work - thanks to this, I was noticed and promoted to the IT administrator with the duties of the head of the department. Over the years I have developed many applications to "make life easier".',
+    type: "text",
+    title: "Experience",
+    description:
+      'For three years I worked as a Logistics Specialist in one of the largest automotive companies (Simoldes Plasticos). Programming skills allowed me to automate logistics work - thanks to this, I was noticed and promoted to the IT administrator with the duties of the head of the department. Over the years I have developed many applications to "make life easier".',
   },
   {
-    type: 'list',
-    title: 'Experience Summary',
-    list: ['IT Administrator (Simoldes Plasticos)', 'Developer (Self-employment)', 'Logistic Specialist (Simoldes Plasticos)', 'Junior Logisitic Specialist (Simoldes Plasticos)'],
+    type: "list",
+    title: "Experience Summary",
+    list: [
+      "IT Administrator (Simoldes Plasticos)",
+      "Developer (Self-employment)",
+      "Logistic Specialist (Simoldes Plasticos)",
+      "Junior Logisitic Specialist (Simoldes Plasticos)",
+    ],
   },
   {
-    type: 'text',
-    title: 'Ambitions, Plans',
-    description: 'As a curious and ambitious person, I would like to develop my skills in the most universal way. That\'s why my focus is on becoming a fullstack developer. Which would allow me to develop not only in one direction. To make this a reality in the near future, I plan to add node.js to my technology stack.',
-  }
-]
+    type: "text",
+    title: "Ambitions, Plans",
+    description:
+      "As a curious and ambitious person, I would like to develop my skills in the most universal way. That's why my focus is on becoming a fullstack developer. Which would allow me to develop not only in one direction. To make this a reality in the near future, I plan to add node.js to my technology stack.",
+  },
+];
 
 export const recentProject = [
   {
-    id: 'Pizzeria',
-    shortDescription: 'Side for the restaurant with orders online and booking tables',
-    description: 'Full responsive website for restaurant, it has the online orders, tabels booking, all with API communication. It has been developed in Vanilla JS - in OOP methodology.',
-    gitHub: 'https://github.com/BeniaminGuras/Pizzeria',
-    img:'pizzeria'
+    id: "Pizzeria",
+    shortDescription:
+      "Side for the restaurant with orders online and booking tables",
+    description:
+      "Full responsive website for restaurant, it has the online orders, tabels booking, all with API communication. It has been developed in Vanilla JS - in OOP methodology.",
+    gitHub: "https://github.com/BeniaminGuras/Pizzeria",
+    img: "pizzeria",
   },
   {
-    id: 'Waiters App',
-    shortDescription: 'Web Application for waiters',
-    description: 'Website Application that allows waiters to manage the table status. It has been written with React, React Router, React Bootstrap, Redux. It\'s dynamically communicating the changes with API.',
-    gitHub: 'https://github.com/BeniaminGuras/waiters_app',
-    img:'waiters'
+    id: "Waiters App",
+    shortDescription: "Web Application for waiters",
+    description:
+      "Website Application that allows waiters to manage the table status. It has been written with React, React Router, React Bootstrap, Redux. It's dynamically communicating the changes with API.",
+    gitHub: "https://github.com/BeniaminGuras/waiters_app",
+    img: "waiters",
   },
   {
-    id: 'Form Project',
-    shortDescription: 'Demo of conditional form build with React',
-    description: 'Demo of form that generates the questions conditianlly. After submiting it checks the correctness of filled inputs - if it\'s ok it starts communication with API. Demo is created in React.',
-    gitHub: 'https://github.com/BeniaminGuras/form_project',
-    img:'form'
+    id: "Form Project",
+    shortDescription: "Demo of conditional form build with React",
+    description:
+      "Demo of form that generates the questions conditianlly. After submiting it checks the correctness of filled inputs - if it's ok it starts communication with API. Demo is created in React.",
+    gitHub: "https://github.com/BeniaminGuras/form_project",
+    img: "form",
   },
   {
-    id: 'To do list',
-    shortDescription: 'Application to create dynamic to do list',
-    description: 'On the website we can dynamically generate categories and a lists of to do tasks with customizable icons. It has been created with React, React Router, Redux.',
-    gitHub: 'https://github.com/BeniaminGuras/To-do-list',
-    img:'to_do_list'
+    id: "To do list",
+    shortDescription: "Application to create dynamic to do list",
+    description:
+      "On the website we can dynamically generate categories and a lists of to do tasks with customizable icons. It has been created with React, React Router, Redux.",
+    gitHub: "https://github.com/BeniaminGuras/To-do-list",
+    img: "to_do_list",
   },
   {
-    id: 'Autocompliter',
-    shortDescription: 'Component to autocomplete text and others',
-    description: 'Component accepts the list of elements that will be searched for a writen phrase. After finding a match it allows the client to select and automatically add the word or number as a tag. It has been created with React.',
-    gitHub: 'https://github.com/BeniaminGuras/Autocompliter-react',
-    img:'autocompliter'
+    id: "Autocompliter",
+    shortDescription: "Component to autocomplete text and others",
+    description:
+      "Component accepts the list of elements that will be searched for a writen phrase. After finding a match it allows the client to select and automatically add the word or number as a tag. It has been created with React.",
+    gitHub: "https://github.com/BeniaminGuras/Autocompliter-react",
+    img: "autocompliter",
   },
-]
+];
 
 export const contact = {
-  email: 'gurasbeniamin@gmail.com',
-  phone: '+48517435956',
-}
+  email: "gurasbeniamin@gmail.com",
+  phone: "+48517435956",
+};
